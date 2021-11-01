2c425991c5ef1fb36c57699f8ab9a412c5fa94c9
wjsdawn <1666938053@qq.com>
2021-11-01 16:22:27 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:5000/';
+createApp(App).use(store).use(VueAxios,axios).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,33 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="getData">get data</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {AxiosInstance as axios} from "axios";
 
 export default {
   name: 'Home',
+  data: function(){
+    return{
+      serverResponse:String
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods:{
+    getData(){
+      this.axios.post('http://127.0.0.1:5000/msg').then(res=>{
+        let message = res.data.msg
+        this.serverResponse = message
+        alert('success'+res.status + ',' + res.data + ',' + message)
+      });
+    }
   }
 }
 </script>
