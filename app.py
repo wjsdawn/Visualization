33bf76c6693a200be581da70097edae1a22dd909
guohao225 <guohao225@163.com>
2021-11-09 19:45:37 +0800

    提取车辆轨迹数据

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+import pymongo
+import pickle
+
+client = pymongo.MongoClient(host='localhost', port=27017)
+db = client.CarRoute
+collection = db.get_collection("cars")
 
 app = Flask(__name__)
 
@@ -8,6 +14,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/send_data')
+def send_data():
+    coll = collection.find_one()
+    df = pickle.loads(coll["route"])
+    arr_point = df.to_json(orient="values")
+    data = {
+        "id": coll["name"],
+        "begin_pos": coll["begin_pos"],
+        "end_pos": coll["end_pos"],
+        "route": arr_point
+    }
+    return data
 
 
 @app.route('/msg', methods=['GET', 'POST'])
