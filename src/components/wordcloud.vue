31fcf0c509023ed5d6339eece201e84c8e6cd674
wangkieme <1737456409@qq.com>
2021-12-20 18:46:52 +0800

    dd

--- src/components/wordcloud.vue
+++ src/components/wordcloud.vue
@@ -1,49 +1,24 @@
 <template>
-    <div id="app1">
-    词云图
-    </div>
+  <div>
+  
+    <el-image :src="srcdata"></el-image>
+    <!-- <img :src="data:image/jpg;base64,这里是base64的编码"/> -->
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      srcdata: "data:image/jpg;base64,",
+    };
+  },
+  mounted() {
+      this.axios.post("http://127.0.0.1:5000/worldcolud").then((res)=>{
+          this.srcdata = this.srcdata+res.data
+      })
+  }
+};
+</script>
 
-//<script>    
-//import * as echarts from 'echarts';
 
-//export default {
-//    mounted(){
-// var myChart = echarts3.init(document.getElementById('app1'));
-// var keywords = [{"name":"成都市双流区","value":2.64},
-//                 {"name":"成都市双流区公黄路","value":4.03},
-//                 {"name":"成都市双流区剑南大道南段辅路","value":24.95},
-//                 {"name":"成都市双流区武汉路西段辅路","value":4.04},]
-// var option = {
-//             series: [{
-//                 type: 'wordCloud',
-//                 sizeRange: [15, 80],
-//                 rotationRange: [0, 0],
-//                 rotationStep: 45,
-//                 gridSize: 8,
-//                 shape: 'pentagon',
-//                 width: '100%',
-//                 height: '100%',
-//                 textStyle: {
-//                     normal: {
-//                         color: function () {
-//                             return 'rgb(' + [
-//                                 Math.round(Math.random() * 160),
-//                                 Math.round(Math.random() * 160),
-//                                 Math.round(Math.random() * 160)
-//                             ].join(',') + ')';
-//                         }
-//                     }
-//                 },
-//                 data: keywords
-//             }]
-//         };
-    //使用刚指定的配置项和数据显示图表。
-//     myChart.setOption(option);
-//     window.addEventListener("resize", function() {
-//         myChart.resize();
-//     });
- 
-// }
-// }
-// </script>
+
